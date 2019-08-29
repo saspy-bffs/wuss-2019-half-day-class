61f68d92efc08796f06b5f2c862f5b76dc31a66d
ilankham <isaiah.lankham@gmail.com>
2019-08-28 20:33:19 -0700

    Add Solution Files

---
+++ Solution_File_B-Common_Python_Data_Structures.py
@@ -0,0 +1,199 @@
+# Everything is better with friends: Executing SAS® code in Python scripts with
+# SASPy, and turbocharging your SAS programming with open-source tooling
+#
+# Half-day class, Western Users of SAS Software (WUSS) 2019
+
+
+###############################################################################
+# Exercises 3-5: Common Python Data Structures                                #
+###############################################################################
+
+# Lines 12-13 load modules needed for exercises and should be left as-is
+from class_setup import print_with_title
+from pandas import DataFrame
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 3. [Python] Define a list object                                    #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+hello_world_list = ['Hello', 'list']
+print_with_title(hello_world_list, 'The value of hello_world_list:')
+print_with_title(type(hello_world_list), 'The type of hello_world_list:')
+
+# Print out the initial element of the list
+print(hello_world_list[0])
+
+# Print out the final element of the list
+print(hello_world_list[1])
+
+# Create a list of length five, and print its middle elements
+new_list = list(range(5))
+print(new_list[2])
+
+
+# Notes:
+#
+# 1. A list object named hello_world_list with two values is created, and the
+#    following are printed:
+#    * the value of the list
+#    * its type (which is <class 'list'>)
+#
+# 2. Lists are the most fundamental Python data structure and are related to
+#    SAS data-step arrays. Values in lists are always kept in insertion order,
+#    meaning the order they appear in the list's definition, and they can be
+#    individually accessed using numerical indexes within bracket notation:
+#    * hello_world_list[0] returns 'Hello'
+#    * hello_world_list[1] returns 'list'
+#
+# 3. This example illustrates another way Python syntax differs from SAS:
+#    * The left-most element of a list is always at index 0. Unlike SAS,
+#      customized indexing is only available for more sophisticated data
+#      structures in Python (e.g., a dictionary, as in the next example).
+#
+# 4. For extra credit, try any or all of the following:
+#    * Print out the initial element of the list.
+#    * Print out the final element of the list.
+#    * Create a list of length five, and print its middle elements.
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 4. [Python] Define a dict object                                    #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+hello_world_dict = {
+    'salutation'      : ['Hello'       , 'dict'],
+    'valediction'     : ['Goodbye'     , 'list'],
+    'part of speech'  : ['interjection', 'noun'],
+}
+print_with_title(hello_world_dict, 'The value of hello_world_dict:')
+print_with_title(type(hello_world_dict), 'The type of hello_world_dict:')
+
+# Print out the list with key 'salutation'
+print(hello_world_dict['salutation'])
+
+# Print out the initial element in the list associated with key 'valediction'
+print(hello_world_dict['valediction'][0])
+
+# Print out the final element in the list associated with key 'part of speech'
+print(hello_world_dict['part of speech'][1])
+
+
+# Notes:
+# 1. A dictionary (dict for short) object named hello_world_dict with three
+#    key-value pairs is created, and the following are printed:
+#    * the value of the dictionary
+#    * its type (which is <class 'dict'>)
+#
+# 2. Dictionaries are another fundamental Python data structure and are related
+#    to SAS formats and data-step hash tables. Dictionaries are more generally
+#    called associative arrays or maps because they map keys (appearing before
+#    the colons) to values (appearing after the colons). In other words, the
+#    value associated with each key can be accessed using bracket notation:
+#    * hello_world_dict['salutation'] returns ['Hello', 'dict']
+#    * hello_world_dict['valediction'] returns ['Goodbye', 'list']
+#    * hello_world_dict['part of speech'] returns ['interjection', 'noun']
+#
+# 3. Whenever indexable data structures are nested in Python, indexing methods
+#    can be combined. E.g., hello_world_dict['salutation'][0] is the same as
+#    ['Hello', 'dict'][0], which returns 'Hello'.
+#
+# 4. When using older versions of Python, the print order of key-value pairs
+#    may not match insertion order, meaning the order key-value pairs are
+#    listed when the dictionary is created. However, as of Python 3.7 (released
+#    in June 2018), insertion order is preserved.
+#
+# 5. For extra credit, try any or all of the following:
+#    * Print out the list with key 'salutation'.
+#    * Print out the initial element in the list associated with key
+#      'valediction'.
+#    * Print out the final element in the list associated with key 'part of
+#      speech'.
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 5. [Python w/ pandas] Define a DataFrame object                     #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+hello_world_df = DataFrame(
+    {
+        'salutation':     ['Hello'      , 'DataFrame'],
+        'valediction':    ['Goodbye'    , 'dict'],
+        'part of speech': ['exclamation', 'noun'],
+    }
+)
+print_with_title(hello_world_df, 'The value of hello_world_df:')
+print_with_title(hello_world_df.shape, 'The shape of hello_world_df:')
+hello_world_df.info()
+print_with_title('', linebreaks_before=0, linebreaks_after=0)
+
+
+# Print out the column with key 'salutation'
+print(hello_world_df['salutation'])
+
+# Print out the initial element in the column with key 'valediction'
+print(hello_world_df['valediction'][0])
+
+# Print out the final element in the column with key 'part of speech'
+print(hello_world_df['part of speech'][1])
+
+
+# Notes:
+#
+# 1. A DataFrame (df for short) object named hello_world_df with dimensions 2x3
+#    (2 rows by 3 columns) is created, and the following are printed:
+#    * the value of the DataFrame
+#    * the number of rows and columns in hello_world_df
+#    * some information about it, which is obtained by hello_world_df calling
+#      its info method (meaning a function whose definition is nested in it)
+#
+# 2. Since DataFrames are not built into Python, we had to import their
+#    definition from the pandas module at the beginning of this file. Like
+#    their R counterpart, DataFrames are two-dimensional arrays of values that
+#    can be thought of like SAS datasets. However, while SAS datasets are
+#    typically only accessed from disk and processed row-by-row, DataFrames are
+#    loaded into memory all at once. This means values in DataFrames can be
+#    randomly accessed, but it also means the size of DataFrames can't grow
+#    beyond available memory.
+#
+# 3. The dimensions of the DataFrame are determined as follows:
+#    * The keys 'salutation', 'valediction', and 'part of speech' of the
+#      dictionary passed to the DataFrame constructor function become column
+#      labels.
+#    * Because each key maps to a list of length two, each column will be two
+#      elements tall (with an error occurring if the lists are not of
+#      non-uniform length).
+#
+# 4. This example gives one option for building a DataFrame, but the
+#    constructor function can also accept many other object types, including
+#    another DataFrame.
+#
+# 5. For extra credit, try any or all of the following (keeping in mind that
+#    DataFrames can be indexed like dictionaries):
+#    * Print out the column with key 'salutation'.
+#    * Print out the initial element in the column with key 'valediction'.
+#    * Print out the final element in the column with key 'part of speech'.
---
+++ Solution_File_C-SASPy_Data_Round_Trip.py
@@ -0,0 +1,256 @@
+# Everything is better with friends: Executing SAS® code in Python scripts with
+# SASPy, and turbocharging your SAS programming with open-source tooling
+#
+# Half-day class, Western Users of SAS Software (WUSS) 2019
+
+
+###############################################################################
+# Examples 6-9: SASPy Data Round Trip                                         #
+###############################################################################
+
+# Lines 12-13 load modules needed for exercises and should be left as-is
+from class_setup import print_with_title
+from saspy import SASsession
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 6. [Python w/ saspy] Connect to a SAS kernel                        #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+sas = SASsession()
+print_with_title(type(sas), 'The type of SAS session object sas:')
+
+
+# Notes:
+#
+# 1. A SASsession object named sas is created, and the following are printed:
+#    * confirmation a SAS session has been established
+#    * the type of object sas (which is saspy.sasbase.SASsession)
+#
+# 2. As with the DataFrame object type above, SASsession is not built into
+#    Python, so we had to import its definition from the saspy module at the
+#    beginning of this file.
+#
+# 3. All subsequent exercises in this file will assume the object sas exists,
+#    so please don't comment out the line creating it.
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 7. [Python w/ pandas & saspy] Load a SAS dataset into a DataFrame   #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+fish_df = sas.sasdata2dataframe(table='fish', libref='sashelp')
+print_with_title(fish_df, 'The value of fish_df:')
+print_with_title(
+    fish_df.describe(),
+    'The Python equivalent of PROC MEANS using fish_df:'
+)
+print_with_title(fish_df.head(), 'The first five rows of fish_df:')
+
+#  Pass a numerical parameter to the head method
+print(fish_df.head(4))
+
+#  Change the head method to tail
+print(fish_df.tail())
+
+#  View other portions of fish_df
+print(fish_df.iloc[0:2, 1:4])
+
+
+# Notes:
+#
+# 1. A DataFrame object named fish_df with dimensions 159x7 (159 rows and 7
+#    columns) is created from the SAS dataset fish in the sashelp library, and
+#    the following are printed:
+#    * the type of object fish_df (which is
+#      <class 'pandas.core.frame.DataFrame'>)
+#    * the first five rows of fish_df, which are at row indices 0 through 4
+#      since Python uses zero-based indexing
+#    * summary information about the 6 numerical columns of fish_df, which is
+#      obtained by fish_df calling its describe method (the pandas equivalent
+#      of the SAS MEANS procedure)
+#
+# 2. The sas object represents a connection to a SAS session and was created
+#    in a previous exercise. Here, sas calls its sasdata2dataframe method to
+#    access the SAS library sashelp defined within this SAS session and to load
+#    the entire contents of SAS dataset sashelp.fish into the DataFrame
+#    fish_df.
+#
+# 3. All subsequent exercises in this file will assume the object fish_df
+#    exists, so please don't comment out the line creating it.
+#
+# 4. For extra credit, try the following:
+#    * Pass a numerical parameter to the head method to see a different number
+#      of rows (e.g., fish_df.head(4)).
+#    * Change the head method to tail to see a different part of the dataset.
+#    * To view other portions of fish_df, explore the more advanced indexing
+#      methods loc and iloc explained at
+#      https://brohrer.github.io/dataframe_indexing.html.
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 8. [Python w/ pandas] Manipulate a DataFrame                        #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+fish_df_g   = fish_df.groupby('Species')
+fish_df_gs  = fish_df_g['Weight']
+fish_df_gsa = fish_df_gs.agg(['count', 'std', 'mean', 'min', 'max'])
+print_with_title(
+    fish_df_gsa,
+    'The Python equivalent of PROC MEANS with CLASS and VAR statements:'
+)
+
+# Move around and/or remove functions used for aggregation
+fish_df_gsa = fish_df_gs.agg(['min', 'mean', 'max'])
+print(fish_df_gsa)
+
+# Change the variable whose values are summarized to 'Width'
+fish_df_gs  = fish_df_g['Width']
+fish_df_gsa = fish_df_gs.agg(['count', 'std', 'mean', 'min', 'max'])
+print(fish_df_gsa)
+
+# Print out the results of using the one-liner version
+print(
+    fish_df.groupby('Species')['Weight'].agg(
+           ['count', 'std', 'mean', 'min', 'max']
+    )
+)
+
+
+# Notes:
+#
+# 1. The DataFrame fish_df, which was created in an exercise above from the SAS
+#    dataset sashelp.fish, is manipulated, and the following is printed:
+#    * a table giving the number of rows, standard deviation, mean, min, and
+#      max of Weight in fish_df when aggregated by Species
+#
+# 2. This is accomplished by creating a series of new DataFrames:
+#    * The DataFrame fish_df_g is created from fish_df using the groupby method
+#      to group rows by values in column 'Species'.
+#    * The DataFrame fish_df_gs is created from fish_df_g by extracting the
+#      'Weight' column using bracket notation.
+#    * The DataFrame fish_df_gsa is created from fish_df_gs using the agg
+#      method to aggregate by the functions in the list ['count', 'std',
+#      'mean', 'min', 'max'].
+#
+# 3. Identical results could be obtained using the following SAS code:
+#        proc means data=sashelp.fish std mean min max;
+#             class species;
+#             var Weight;
+#        run;
+#    However, while PROC MEANS operates on SAS datasets row-by-row from disk,
+#    DataFrames are stored entirely in main memory. This allows any number of
+#    DataFrame operations to be combined for on-the-fly reshaping using "method
+#    chaining." In other words, fish_df_gsa could have instead been created
+#    with the following one-liner, which avoids the need for intermediate
+#    DataFrames (and thus executes much more quickly):
+#        fish_df_gsa = fish_df.groupby('Species')['Weight'].agg(
+#            ['count', 'std', 'mean', 'min', 'max']
+#        )
+#
+# 3. All subsequent exercises in this file will assume the object fish_df_gsa
+#    exists, so please don't comment out the line(s) creating it.
+#
+# 4. For extra credit, try the following:
+#    * Move around and/or remove functions used for aggregation, and see how
+#      the output changes.
+#    * Change the variable whose values are summarized to 'Width'.
+#    * Print out the results of using the one-liner version.
+
+
+
+
+###############################################################################
+#                                                                             #
+# Example 9. [Python w/ pandas & saspy] Load a DataFrame into a SAS dataset;  #
+# execute SAS code                                                            #
+#                                                                             #
+# Instructions: Uncomment the code immediately below, and then execute        #
+#                                                                             #
+###############################################################################
+
+
+# Original Version
+sas.dataframe2sasdata(fish_df_gsa, table="fish_sds_gsa", libref="Work")
+sas_submit_return_value = sas.submit(
+    '''
+        PROC PRINT DATA=fish_sds_gsa;
+        RUN;
+    ''',
+    results='TEXT'
+)
+sas_submit_results = sas_submit_return_value['LST']
+print_with_title(
+    sas_submit_results,
+    'SAS results from PROC PRINT applies to new SAS dataset Work.fish_sds_gsa:'
+)
+
+#  Print out the SAS log
+print(sas_submit_return_value['LOG'])
+
+#  Change the SAS procedure
+print(
+    sas.submit(
+        '''
+            PROC CONTENTS DATA=fish_sds_gsa;
+            RUN;
+        ''',
+        results='TEXT'
+    )['LST']
+)
+
+
+# Notes:
+#
+# 1. The DataFrame fish_df_gsa, which was created in an exercise above from the
+#    SAS dataset sashelp.fish, is used to create the new SAS dataset
+#    Work.fish_sds_gsa. The SAS PRINT procedure is then called, and the
+#    following is printed:
+#    * the output returned by PROC PRINT
+#
+# 2. The sas object, which was created in a cell above, is a persistent
+#    connection to a SAS session, and two of its methods are used as follows:
+#    * The dataframe2sasdata method writes the contents of the DataFrame
+#      fish_df_gsa to the SAS dataset fish_sds_gsa stored in the Work library.
+#      (Note: The row indexes of the DataFrame fish_df_gsa are lost when the
+#      SAS dataset fish_sds_gsa is created.)
+#    * The submit method is used to submit the PROC PRINT step to the SAS
+#      kernel, and a dictionary is returned with the following two key-value
+#      pairs:
+#      - sas_submit_return_value['LST'] is a string comprising the results from
+#        executing PROC PRINT, which will be in plain text because the
+#        results='TEXT' was used
+#      - sas_submit_return_value['LOG'] is a string comprising the plain-text
+#        log resulting from executing PROC PRINT
+#
+# 3. Python strings surrounded by single quotes (e.g., 'Hello, World!') cannot
+#    be written across multiple lines of code, whereas strings surrounded by
+#    triple quotes (e.g., the argument to the submit method) can.
+#
+# 4. For extra credit, try the following:
+#    * Print out the SAS log.
+#    * Change the SAS procedure used to interact with SAS dataset
+#      Work.fish_sds_gsa (e.g., try PROC CONTENTS).
